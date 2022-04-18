fb87ae928af96a1b84f0f094f599cdbdde46c025
Ruslan <tagiyevazenadua@gmail.com>
2022-04-18 17:13:57 +0300

    value

--- src/nats_client/client.rs
+++ src/nats_client/client.rs
@@ -20,7 +20,7 @@ impl NatsClient {
     {
         let opts = options.into();
         let tcp_stream =
-            NatsClientInner::try_connect(opts.clone(), &opts.cluster_uris.0, false).await?;
+            NatsClientInner::try_connect(opts.clone(), &opts.cluster_uris.0, 10).await?;
         let (sink, stream) = NatsTcpStream::new(tcp_stream).await.split();
 
         let version = 1;
