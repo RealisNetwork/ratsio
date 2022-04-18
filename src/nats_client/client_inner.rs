b030dd89b19829fa556333cb2fa9cbbd79740555
Ruslan <tagiyevazenadua@gmail.com>
2022-04-18 17:04:36 +0300

    retrying count

--- src/nats_client/client_inner.rs
+++ src/nats_client/client_inner.rs
@@ -25,8 +25,9 @@ impl NatsClientInner {
     pub(in crate::nats_client) async fn try_connect(
         opts: NatsClientOptions,
         cluster_uris: &[String],
-        keep_retrying: bool,
+        keep_retrying: u16,
     ) -> Result<TcpStream, RatsioError> {
+        let mut retrying_count = keep_retrying;
         let valid_addresses = cluster_uris
             .iter()
             .flat_map(|raw_uri| {
@@ -61,8 +62,9 @@ impl NatsClientInner {
                 }
             }
             error!("Unable to connect to any of the Nats servers, will retry again.");
-            if keep_retrying {
+            if retrying_count > 0 {
                 let _ = Delay::new(Duration::from_millis(opts.reconnect_timeout)).await;
+                retrying_count -= 1;
             } else {
                 return Err(RatsioError::NoRouteToHostError);
             }
@@ -267,7 +269,7 @@ impl NatsClientInner {
             return Err(RatsioError::CannotReconnectToServer);
         };
         let tcp_stream =
-            Self::try_connect(self.opts.clone(), &self.opts.cluster_uris.0, true).await?;
+            Self::try_connect(self.opts.clone(), &self.opts.cluster_uris.0, 10).await?;
         let (sink, stream) = NatsTcpStream::new(tcp_stream).await.split();
         *self.conn_sink.lock().await = sink;
         *self.reconnect_version.write().await += 1;
